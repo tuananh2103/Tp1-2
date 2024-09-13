b9a63dd41d3f3a97791fbec26be4a1430e855570
tuananh2103 <anh.nguyen@etudiant.univ-rennes1.fr>
2024-09-13 12:47:00 +0200

    tuan anh

--- PRAV/TP1-2/src/Point2D.java
+++ PRAV/TP1-2/src/Point2D.java
@@ -18,7 +18,21 @@ public class Point2D {
         this.x = point.x;
         this.y = point.y;
     }
+    // Méthode equals pour comparer deux points
+    public boolean equals(Point2D other) {
+        if (this == other) {
+            return true; // Si les deux objets sont identiques
+        }
+        // Vérifie si les coordonnées x et y sont égales
+        return this.x == other.x && this.y == other.y;
+    }
 
+    // Méthode toString pour représenter le point sous forme de chaîne de caractères
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+    
     public double getX() {
         return x;
     }
